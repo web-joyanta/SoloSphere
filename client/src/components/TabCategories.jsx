c78eb2c6e9e31e9eb2ea49fbf20248d9ec612dfb
web-joyanta <web.joyanta@gmail.com>
2025-08-23 00:21:29 +0600

    home page tab list button click to dynamic data show

--- client/src/components/TabCategories.jsx
+++ client/src/components/TabCategories.jsx
@@ -2,8 +2,20 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
 import JobCard from './JobCard'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const TabCategories = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    FetchAllJobs()
+  }, []);
+
+  const FetchAllJobs = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/jobs`)
+    setJobs(data);
+  }
   return (
     <Tabs>
       <div className=' container px-6 py-10 mx-auto'>
@@ -25,25 +37,19 @@ const TabCategories = () => {
         </div>
         <TabPanel>
           <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            <JobCard />
-            <JobCard />
-            <JobCard />
-            <JobCard />
+            {jobs.filter(job => job.category === "Web Development").map(job => <JobCard key={job._id} job={job} />)}
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            <JobCard />
-            <JobCard />
+            {jobs.filter(job => job.category === "Graphics Design").map(job => <JobCard key={job._id} job={job} />)}
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            <JobCard /> <JobCard />
-            <JobCard /> <JobCard />
-            <JobCard /> <JobCard />
+            {jobs.filter(job => job.category === "Digital Marketing").map(job => <JobCard key={job._id} job={job} />)}
           </div>
         </TabPanel>
       </div>
@@ -51,4 +57,4 @@ const TabCategories = () => {
   )
 }
 
-export default TabCategories
+export default TabCategories;
