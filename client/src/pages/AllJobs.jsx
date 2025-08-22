8b22a21155bab22e6e6b90a131844575117a0b77
web-joyanta <web.joyanta@gmail.com>
2025-08-23 00:08:48 +0600

    all jobs map jobCard data show

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -15,7 +15,6 @@ const AllJobs = () => {
     setJobs(data);
   }
 
-  console.log(jobs);
   return (
     <div className='container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between'>
       <div>
@@ -62,14 +61,7 @@ const AllJobs = () => {
           <button className='btn'>Reset</button>
         </div>
         <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
-          <JobCard />
+          {jobs.map(job => <JobCard key={job._id} job={job} />)}
         </div>
       </div>
     </div>
