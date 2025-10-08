4fa92e8be6113595d93aec1a5b203af86f313945
web-joyanta <web.joyanta@gmail.com>
2025-10-09 00:28:32 +0600

    AllJobs component pagination button (previous,[1, 2, 3, 4, 5], next) setup

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -4,11 +4,13 @@ import JobCard from '../components/JobCard'
 import axios from 'axios'
 
 const AllJobs = () => {
+  const pages = [1, 2, 3, 4, 5]
+
   const [jobs, setJobs] = useState([]);
   const [filter, setFilter] = useState('');
   const [search, setSearch] = useState('');
   const [sort, setSort] = useState('');
-  console.log(search)
+
   useEffect(() => {
     FetchAllJobs()
   }, [filter, search, sort]);
@@ -74,6 +76,62 @@ const AllJobs = () => {
         <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {jobs.map(job => <JobCard key={job._id} job={job} />)}
         </div>
+
+        {/* Pagination button start */}
+        <div className='flex justify-center mt-12'>
+          <button className='px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white'>
+            <div className='flex items-center -mx-1'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='w-6 h-6 mx-1 rtl:-scale-x-100'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M7 16l-4-4m0 0l4-4m-4 4h18'
+                />
+              </svg>
+
+              <span className='mx-1'>previous</span>
+            </div>
+          </button>
+
+          {pages.map(btnNum => (
+            <button
+              key={btnNum}
+              className={`hidden px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            >
+              {btnNum}
+            </button>
+          ))}
+
+          <button className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'>
+            <div className='flex items-center -mx-1'>
+              <span className='mx-1'>Next</span>
+
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='w-6 h-6 mx-1 rtl:-scale-x-100'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M17 8l4 4m0 0l-4 4m4-4H3'
+                />
+              </svg>
+            </div>
+          </button>
+        </div>
+        {/* Pagination button end */}
+
       </div>
     </div>
   )
